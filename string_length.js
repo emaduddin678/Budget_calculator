c6a2062b16538dc161af375640a3a56045a1e090
emaduddin678 <emaduddin678@gmail.com>
2022-05-30 15:14:43 +0600

    string length done

--- string_length.js
+++ string_length.js
@@ -2,3 +2,26 @@ let str = "Apple, Banana, Mango";
 console.log(`This is the length of String = ${str.length}`);
 console.log(str.slice(7, 13));
 console.log(str.substr(7,11))
+let newStr = str.replace("Mango","Litchi");
+console.log(`Main string = ${str}`)
+console.log(`Replace string = ${newStr}`)
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str.concat("  =  ",newStr))
+console.log(str.charAt(0))
+console.log(str.charAt(1))
+console.log(str.charAt(3))
+
+function lengthOfStr(string){
+    let length = 0;
+    while (true){
+        if(string.charAt(length) == ''){
+            break;
+        }else{
+            length++;
+        }
+    }
+    console.log(length);
+}
+
+lengthOfStr(str);
